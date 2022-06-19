e5ca346990d59e28dd183db18d929b1dbf0df449
Mathias <maticrocco330@gmail.com>
2022-06-19 20:43:52 -0300

    super actualizacion

--- js/main.js
+++ js/main.js
@@ -50,5 +50,11 @@ function main() {
 }
 main();
 
-//Galeria
+//Modal
 
+
+let close_button = document.getElementById('close-button');
+close_button.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.getElementById("window-notice").style.display = "none";
+});
